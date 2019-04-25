a75ea0fc3750963baa2984f7c82f02b77b27a3d9
Richard Gross <richard.gross@maibornwolff.de>
2019-04-25 09:31:05 +0200

    add functionality to read input streams

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 ## Usage
 
-Run `./gradlew build`
+Run `./gradlew build`, then you can use the jar in `build/libs` like so:
+
+* Create a file with test contents: `echo "foo\nbar" > foo.txt`
+* Pipe file contents to the jar: `java -jar build/libs/sandboxCli-1.0-SNAPSHOT.jar < foo.txt`
+* Pipe file contents to the jar: `cat foo.txt | java -jar build/libs/sandboxCli-1.0-SNAPSHOT.jar`
 
--- src/main/kotlin/de/richargh/sandbox/kotlinPicocliPipes/Cli.kt
+++ src/main/kotlin/de/richargh/sandbox/kotlinPicocliPipes/Cli.kt
@@ -3,19 +3,25 @@ package de.richargh.sandbox.kotlinPicocliPipes
 import picocli.CommandLine.*
 import java.io.IOException
 import java.io.PrintStream
+import java.util.*
 import java.util.concurrent.Callable
 
 @Command(
         name = "Cli",
         description = ["generates cc.JSON from source code"])
-class Cli(private val outputStream: PrintStream): Callable<Void> {
+class Cli(private val out: PrintStream): Callable<Void> {
 
     @Option(names = ["-h", "--help"], usageHelp = true, description = ["displays this help and exits"])
     private var help = false
 
     @Throws(IOException::class)
     override fun call(): Void? {
-        outputStream.println("Supppp")
+        out.println("Supppp")
+
+        val sc = Scanner(System.`in`)
+        out.println("Passed in was:")
+        while (sc.hasNextLine()) out.println(sc.nextLine())
+
         return null
     }
 }
